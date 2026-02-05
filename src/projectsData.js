3484dccf704edb1ec2729723bbd9e056d5078177
Jayraj <dfctv17@gmail.com>
2026-02-05 21:19:26 +0530

    aesthetics

--- src/projectsData.js
+++ src/projectsData.js
@@ -2,16 +2,23 @@ const projects = [
   {
     id: 1,
     title: "Hand-Painted Ceramic Plate",
-    thumbnail: "/images/plate-thumb.jpeg",
+    thumbnail: "/images/ceramic.jpg",
     details: "This plate was created using acrylic glass paints. Inspired by traditional floral motifs, it features vibrant blues and gold leaf accents.",
     category: "Craft"
   },
   {
     id: 2,
-    title: "Abstract Watercolor Study",
-    thumbnail: "/images/arthu.jpeg",
+    title: "Abstract Watercolor",
+    thumbnail: "/images/water.jpeg",
     details: "A deep dive into color blending and water control. This piece represents the chaos of a summer storm.",
     category: "Art"
+  },
+  {
+    id: 3,
+    title: "Fraction Fish",
+    thumbnail: "/images/fraction.jpeg",
+    details: "A collection of colorful aquatic beings, each creatively segmented to show how different parts make a whole.",
+    category: "Craft"
   }
 ];
 
